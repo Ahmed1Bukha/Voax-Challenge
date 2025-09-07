2ba5475aaed97444d5561bb06a4f2a5e4ba9e520
AhmedBukha <56551464+Ahmed1Bukha@users.noreply.github.com>
2025-09-07 20:49:55 +0300

    Unified the get and post request using the env variable

--- src/routes/v1/blobRoute.ts
+++ src/routes/v1/blobRoute.ts
@@ -1,23 +1,34 @@
 import { Router } from "express";
 import BlobController from "../../controllers/blobController";
+import { StorageType } from "../../utils/enums/storageEnums";
+import config from "../../config/config";
 
 const router = Router();
 const blobController = new BlobController();
 
 // Apply the middleware to all blob routes
 
-router.get(`/:id/s3`, (req, res) => blobController.getBlobFromS3(req, res));
-router.post(`/s3`, (req, res) => blobController.putBlobToS3(req, res));
-router.get("/:id/database", (req, res) =>
-  blobController.getBlobFromDatabase(req, res)
-);
-router.post("/database", (req, res) =>
-  blobController.createBlobInDatabase(req, res)
-);
-router.post("/local", (req, res) =>
-  blobController.saveBlobToLocalStorage(req, res)
-);
-router.get("/:id/local", (req, res) =>
-  blobController.getBlobFromLocalStorage(req, res)
-);
+switch (config.storageType) {
+  case StorageType.S3:
+    router.get(`/:id`, (req, res) => blobController.getBlobFromS3(req, res));
+    router.post(`/`, (req, res) => blobController.putBlobToS3(req, res));
+    break;
+  case StorageType.DATABASE:
+    router.get(`/:id/`, (req, res) =>
+      blobController.getBlobFromDatabase(req, res)
+    );
+    router.post(`/`, (req, res) =>
+      blobController.createBlobInDatabase(req, res)
+    );
+    break;
+  case StorageType.LOCAL:
+    router.post(`/`, (req, res) =>
+      blobController.saveBlobToLocalStorage(req, res)
+    );
+    router.get(`/:id`, (req, res) =>
+      blobController.getBlobFromLocalStorage(req, res)
+    );
+    break;
+}
+
 export default router;
